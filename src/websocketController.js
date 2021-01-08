f8c7593ac6e7f3a5884d940c11e2da3fc6014463
raienko <raienko.a@gmail.com>
2021-01-08 03:11:25 +0200

    save current progress

--- src/websocketController.js
+++ src/websocketController.js
@@ -8,32 +8,34 @@ const getRequestHeaders = (request) => {
   return request.httpRequest[symbol];
 }
 
+const extractUserId = (authorizationToken) => authorizationToken;
+
 module.exports = (request) => {
   const headers = getRequestHeaders(request);
   const authorizationToken = headers.authorization;
-  console.log({ authorizationToken });
+  const userId = extractUserId(authorizationToken);
+
   if (!originIsAllowed(request.origin)) {
     // Make sure we only accept requests from an allowed origin
     request.reject();
-    console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+    console.log(`${userId} connection from "${request.origin}" origin rejected.`);
     return;
   }
 
   const connection = request.accept('echo-protocol', request.origin);
-  console.log((new Date()) + ' Connection accepted.');
+  console.log(`${userId} connected`);
 
   connection.on('message', function(message) {
     if (message.type === 'utf8') {
-      console.log('Received Message: ' + message.utf8Data);
+      console.log(`${userId} sent message: ` + message.utf8Data);
       connection.sendUTF(message.utf8Data);
-    }
-    else if (message.type === 'binary') {
-      console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
+    } else if (message.type === 'binary') {
+      console.log(`${userId} sent file of ` + message.binaryData.length + ' bytes');
       connection.sendBytes(message.binaryData);
     }
   });
 
   connection.on('close', function(reasonCode, description) {
-    console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+    console.log(`${userId} disconnected`);
   });
 };
